a2c3ba4e37a859bc5b66566daccb395a95c85c2e
Joseph DeCommer <jdecommer@gmail.com>
2024-02-22 20:09:17 -0500

    unifying styles

--- js/quotes.js
+++ js/quotes.js
@@ -21,6 +21,18 @@ const generateQuote = function() {
         quote: "Be patient with both friends and enemies.",
         attr: "Lao Tzu"
     },
+    {
+        quote: "The secret of happiness, you see is not found in seeking more, but in developing the capacity to enjoy less.",
+        attr: "Socrates"
+    },
+    {
+        quote: "To know what people really think, pay regard to what they do rather than what they say.",
+        attr: "René Descartes"
+    },
+    {
+        quote: "Conquer yourself rather than the world.",
+        attr: "René Descartes"
+    },
 ];
 
     let arrayIdx = Math.floor(Math.random() * quotes.length);

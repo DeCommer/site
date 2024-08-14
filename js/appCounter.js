7f028a3f65042a839a13d120234668997d7736b3
Joseph DeCommer <jdecommer@gmail.com>
2024-08-14 10:39:47 -0400

    added app counter feature

---
+++ js/appCounter.js
@@ -0,0 +1,13 @@
+const totalApps = document.querySelectorAll('.app-link-icon');
+const totalCalculatorApps = document.querySelectorAll('.calculators .app-link-icon');
+const totalToolsApps = document.querySelectorAll('.tools .app-link-icon');
+const totalFunApps = document.querySelectorAll('.fun .app-link-icon');
+
+
+const countApps = (() => {
+        document.getElementById('cal-apps').innerText = `${totalCalculatorApps.length} / ${totalApps.length}`;
+        document.getElementById('tools-apps').innerText = `${totalToolsApps.length} / ${totalApps.length}`;
+        document.getElementById('fun-apps').innerText = `${totalFunApps.length} / ${totalApps.length}`;
+});
+
+countApps();

3518ce4261bac08c8aca779bcba303589b2e6267
Joseph DeCommer <jdecommer@gmail.com>
2024-08-15 15:19:07 -0400

    Added API section

--- js/appCounter.js
+++ js/appCounter.js
@@ -1,8 +1,10 @@
 const totalApps = document.querySelectorAll('.app-link-icon');
 const totalCalculatorApps = document.querySelectorAll('.calculators .app-link-icon');
 const totalToolsApps = document.querySelectorAll('.tools .app-link-icon');
+const totalApiApps = document.querySelectorAll('.api .app-link-icon');
 const totalFunApps = document.querySelectorAll('.fun .app-link-icon');
 
 const calsAppsText = document.getElementById('cal-apps').innerText = `${totalCalculatorApps.length} / ${totalApps.length}`;
 const toolsAppsText = document.getElementById('tools-apps').innerText = `${totalToolsApps.length} / ${totalApps.length}`;
+const apiAppsText = document.getElementById('api-apps').innerText = `${totalApiApps.length} / ${totalApps.length}`;
 const funAppsText = document.getElementById('fun-apps').innerText = `${totalFunApps.length} / ${totalApps.length}`;

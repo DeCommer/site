f1e6829e77bdc7f858059c313b79ff9775e935a1
Joseph DeCommer <jdecommer@gmail.com>
2024-09-08 18:21:24 -0400

    Improvements

--- js/img-modal.js
+++ js/img-modal.js
@@ -2,19 +2,16 @@ const imageModal = document.querySelector('.image-modal');
 const overlay = document.querySelector(".overlay");
 const images = document.querySelectorAll(".gallery__item img");
 let imgSrc;
-
 images.forEach((img) => {
     img.addEventListener("click", (e) => {
         imgSrc = e.target.src;
         overlay.style.display = "block";
-
         imageModal.classList.remove('hidden');
         imageModal.style.position = 'fixed';
         imageModal.style.top = `-${document.body.scrollY}px`;
         imageModal.innerHTML=`
             <img id="1" src="${e.target.src}"  alt="">
-            <button class="close-image-modal">&times</button>`
-
+            <a class="close-image-modal">x</a>`
         const closeImageBtn = document.querySelector('.close-image-modal');
         closeImageBtn.addEventListener('click', () => {
             overlay.style.display = "none";

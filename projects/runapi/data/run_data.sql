6e4e119332714f3b963e3f52bf13dbe7bfee12ba
Joseph DeCommer <jdecommer@gmail.com>
2024-11-16 14:27:01 -0500

    run data update

--- projects/runapi/data/run_data.sql
+++ projects/runapi/data/run_data.sql
@@ -1615,6 +1615,7 @@ INSERT INTO run_data (weekday, month, dom, year, avg_pace, distance, duration, c
 INSERT INTO run_data (weekday, month, dom, year, avg_pace, distance, duration, calories) VALUES ('Friday', 'November', '8', '2024', '8`09``', '4.85', '00:39:34', '592');
 INSERT INTO run_data (weekday, month, dom, year, avg_pace, distance, duration, calories) VALUES ('Monday', 'November', '11', '2024', '8`10``', '6.29', '00:51:25', '799');
 INSERT INTO run_data (weekday, month, dom, year, avg_pace, distance, duration, calories) VALUES ('Tuesday', 'November', '12', '2024', '7`59``', '4.04', '00:32:16', '516');
+INSERT INTO run_data (weekday, month, dom, year, avg_pace, distance, duration, calories) VALUES ('Saturday', 'November', '16', '2024', '8`18``', '6.25', '00:51:57', '935');
 
 
 

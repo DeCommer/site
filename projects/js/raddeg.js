16b7c34ce77ff88f8b7da552f3390051cbf9a733
Joseph DeCommer <jdecommer@gmail.com>
2024-11-20 07:21:38 -0500

    updated run data and other improvements

--- projects/js/raddeg.js
+++ projects/js/raddeg.js
@@ -11,19 +11,65 @@ const resultTempTxt = document.getElementById('result-temp-txt');
 
 let state = 'degrees';
 
+function degToRad() {
+    if(input.value === '') {
+        resultTempTxt.textContent = `0`;
+
+    }else {
+        let deg = input.value
+        toRad = ((deg * Math.PI) / 180).toFixed(3);
+        message.textContent = `${input.value} degrees equals`
+        resultTempTxt.innerHTML = `<p id="result-temp-txt" class="result-temp">${toRad}<span>rad</span></p>`;
+        formula.textContent = `Formula: degrees x π/180 = radians`;
+    }
+}
+
+function radToDeg() {
+    if(input.value === '') {
+        resultTempTxt.textContent = `0`;
+    }else {
+        let rad = input.value
+        toDeg = ((rad * 180) / Math.PI).toFixed(3);
+        message.textContent = `${input.value} radians equals`
+        resultTempTxt.innerHTML = `<p id="result-temp-txt" class="result-temp">${toDeg}<span>deg</span></p>`;
+        formula.textContent = `Formula: radians x 180/π = Degrees°`;
+    }
+}
+
+function clear() {
+    state = 'degrees';
+    degBtn.classList.add('active');
+    radBtn.classList.remove('active');
+    input.value = '';
+    message.textContent = ``;
+    dispType.textContent = 'Deg - Rad';
+    resultTempTxt.textContent = `0`;
+    formula.textContent = "Enter a value to convert";
+}
+
+degBtn.addEventListener('click', () => {
+    degBtn.classList.add('active');
+    radBtn.classList.remove('active');
+    dispType.textContent = 'Deg - Rad'
+    state = 'degrees';
+    if(input.value === '') {
+        resultTempTxt.textContent = `0`;
+    }else {
+        degToRad();
+    }
+});
+
 radBtn.addEventListener('click', () => {
     state = 'radians';
     dispType.textContent = 'Rad - Deg'
     radBtn.classList.add('active');
     degBtn.classList.remove('active');
+    if(input.value === '') {
+        resultTempTxt.textContent = `0`;
+    }else {
+        radToDeg();
+    }
 });
-
-degBtn.addEventListener('click', () => {
-    dispType.textContent = 'Deg - Rad'
-    state = 'degrees';
-    degBtn.classList.add('active');
-    radBtn.classList.remove('active');
-})
 
 enterBtn.addEventListener('click', () => {
     if(input.value === '') {
@@ -32,24 +78,12 @@ enterBtn.addEventListener('click', () => {
             formula.textContent = "Enter a value to convert";
         }, 1500);
     }else if(state === 'degrees') {
-        let deg = input.value
-        toRad = ((deg * Math.PI) / 180).toFixed(3);
-        message.textContent = `${input.value} degrees equals`
-        resultTempTxt.innerHTML = `<p id="result-temp-txt" class="result-temp">${toRad}<span>rad</span></p>`;
-        formula.textContent = `Formula: degrees x π/180 = radians`;
+        degToRad();
     }else if (state === 'radians') {
-        let rad = input.value
-        toDeg = ((rad * 180) / Math.PI).toFixed(3)
-        resultTempTxt.innerHTML = `<p id="result-temp-txt" class="result-temp">${toDeg}<span>deg</span></p>`;
-        formula.textContent = `Formula: radians x 180/π = Degrees°`;
+        radToDeg();
     }
 });
 
 clearBtn.addEventListener('click', () => {
-    state = 'degrees';
-    input.value = '';
-    message.textContent = ``;
-    dispType.textContent = 'Deg - Rad';
-    resultTempTxt.textContent = `0`;
-    formula.textContent = ``;
+    clear();
 });

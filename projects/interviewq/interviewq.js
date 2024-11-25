cb2d936a03fec24609c656a4bcd3db9af36f1374
Joseph DeCommer <jdecommer@gmail.com>
2024-11-25 15:26:47 -0500

    added interview practice app

---
+++ projects/interviewq/interviewq.js
@@ -0,0 +1,29 @@
+fetch("./interviewq.json")
+.then(response => response.json())
+.then(data => iq(data))
+
+const daysRemainingText = document.getElementById('span-days-remaining');
+
+const today = new Date();
+const targetDate = new Date(today.getFullYear(), 11, 2); // Month is zero-based (11 = December)
+if (today > targetDate) {targetDate.setFullYear(targetDate.getFullYear() + 1);}
+const timeDiff = targetDate - today;
+const daysRemaining = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+daysRemainingText.innerHTML = `${daysRemaining}`;
+
+
+function iq(data) {
+    const question = document.getElementById('iq-question');
+    const newQuestion = document.getElementById('new-btn');
+
+    function getNextQuestion() {
+        for(let q in data.questions) {
+            i = Math.floor(Math.random() * 20);
+        }
+        question.innerHTML = `${data.questions[i].question}`
+    }
+    getNextQuestion();
+    newQuestion.addEventListener('click', () => {
+        getNextQuestion()
+    })
+}

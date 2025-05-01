670e86a3e9ff1de2d8a07a1e9050a231391787d0
DeCommer <jdecommer@gmail.com>
2025-05-01 14:59:06 -0400

    reduced runSum by 1 because of error in code

--- projects/runapi/js/main.js
+++ projects/runapi/js/main.js
@@ -154,7 +154,7 @@ const displayRuns = (data) => {
             calSum += run.calories;
             distSum += (run.distance);
     }); 
-    totalRuns.innerHTML = `<p>Total Runs: <span>${formatNumber(Math.round(runSum) - 1)}</span>`;
+    totalRuns.innerHTML = `<p>Total Runs: <span>${formatNumber(Math.round(runSum))}</span>`;
     totalDist.innerHTML = `<p>Distance: <span>${formatNumber(Math.round(distSum) - 31)}</span> mi`;
     totalCal.innerHTML = `<p>Calories: <span>${formatNumber(Math.round(calSum))}</span>`;
 

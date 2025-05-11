4f3b961a54fa4bbe9dd8ceacb794a02f94e8b50b
Joseph DeCommer <jdecommer@gmail.com>
2025-05-11 13:44:05 -0400

    clean up

--- projects/diceroller/dice.js
+++ projects/diceroller/dice.js
@@ -10,24 +10,19 @@ let d2Image = document.getElementById('d-2');
 d2Image.innerHTML = `<img src="../assets/dice/${dice2}.png">`;
 d1Image.innerHTML = `<img src="../assets/dice/${dice1}.png">`;
 
-
 function rollDice() {
     dice1 = Math.floor(Math.random() * 6) + 1;
     dice2 = Math.floor(Math.random() * 6) + 1;
     let total = dice1 + dice2;
-
     for(let i = 0; i <= 6; i ++) {
         d1Image.innerHTML = `<img src="../assets/dice/${dice1}.png">`;
         d2Image.innerHTML = `<img src="../assets/dice/${dice2}.png">`;
     }
-
     if(total === 8 || total === 11) {
         message.innerHTML = `You got an ${total}!`;
     }else {
         message.innerHTML = `You got a ${total}!`;
     }
-
-
 }
 
 rollBtn.addEventListener('click', () => {

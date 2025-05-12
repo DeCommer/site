fe1c65566e7d79999fff2dbd4903d0b8abc535af
Joseph DeCommer <jdecommer@gmail.com>
2025-05-12 17:38:31 -0400

    refactored to eliminate duplicate code

--- projects/diceroller/dice.js
+++ projects/diceroller/dice.js
@@ -1,42 +1,35 @@
-const rollBtn = document.getElementById('roll-btn');
-let message = document.getElementById('messageTxt');
-let doubles = document.getElementById('doublesTxt');
-
-let dice1 = Math.floor(Math.random() * 6) + 1;
-let dice2 = Math.floor(Math.random() * 6) + 1;
-
-let d1Image = document.getElementById('d-1'); 
-let d2Image = document.getElementById('d-2'); 
-
-d2Image.innerHTML = `<img src="../assets/dice/${dice2}.png">`;
-d1Image.innerHTML = `<img src="../assets/dice/${dice1}.png">`;
+const rollBtn = document.getElementById("roll-btn");
+const message = document.getElementById("message-text");
+const special = document.getElementById("special-txt");
 
 function rollDice() {
-    dice1 = Math.floor(Math.random() * 6) + 1;
-    dice2 = Math.floor(Math.random() * 6) + 1;
+    let dice1 = Math.floor(Math.random() * 6) + 1;
+    let dice2 = Math.floor(Math.random() * 6) + 1;
+    let d1Image = document.getElementById('d-1'); 
+    let d2Image = document.getElementById('d-2'); 
     let total = dice1 + dice2;
-    for(let i = 0; i <= 6; i ++) {
-        d1Image.innerHTML = `<img src="../assets/dice/${dice1}.png">`;
+
+    for(let i = 0; i <= 6; i++) {
         d2Image.innerHTML = `<img src="../assets/dice/${dice2}.png">`;
+        d1Image.innerHTML = `<img src="../assets/dice/${dice1}.png">`;
     }
 
     if(dice1 === 1 && dice2 === 1) {
-        doubles.innerHTML = "Snake Eyes!!!";
+        special.innerHTML = `Snake eyes!!!`;
     }else if(dice1 === dice2) {
-        doubles.innerHTML = "Doubles!"
-    }
-    else {
-        doubles.innerHTML = "";
+        special.innerHTML = `Doubles!`;
+    }else {
+        special.innerHTML = ``;
     }
 
     if(total === 8 || total === 11) {
-        message.innerHTML = `You got an ${total}!`;
+        message.innerHTML = `You got an ${total}`;
     }else {
-        message.innerHTML = `You got a ${total}!`;
+        message.innerHTML =`You got a ${total}`;
     }
 }
 rollDice();
 
 rollBtn.addEventListener('click', () => {
     rollDice();
-})
+});

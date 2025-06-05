80766d2df8aa2a47caab6d358f81e9ad8f92b395
Joseph DeCommer <jdecommer@gmail.com>
2025-06-05 19:25:25 -0400

    math error adjustment

--- projects/runapi/js/main.js
+++ projects/runapi/js/main.js
@@ -155,7 +155,7 @@ const displayRuns = (data) => {
             distSum += (run.distance);
     }); 
     totalRuns.innerHTML = `<p>Total Runs: <span>${formatNumber(Math.round(runSum))}</span>`;
-    totalDist.innerHTML = `<p>Distance: <span>${formatNumber(Math.round(distSum) - 31)}</span> mi`;
+    totalDist.innerHTML = `<p>Distance: <span>${formatNumber(Math.round(distSum) + 5)}</span> mi`;
     totalCal.innerHTML = `<p>Calories: <span>${formatNumber(Math.round(calSum))}</span>`;
 
     const milesToVolt = VOLT - (distSum);
